a5db53dab2722acde4723735fc7d841f43780eb1
Kim Thái <thaihk@fanxipan.net>
2025-10-20 23:25:40 +0700

    feat: modal department

--- src/pages/management/system/department/detail.tsx
+++ src/pages/management/system/department/detail.tsx
@@ -1,19 +1,122 @@
-import { useParams } from '@/routes/hooks';
-import { Card, CardContent } from '@/ui/card';
-import type { UserInfo } from '#/entity';
+import { UserInfo } from '@/types/entity';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/ui/dialog';
+import { Form, FormControl, FormField, FormItem, FormLabel } from '@/ui/form';
+import { Input } from '@/ui/input';
+import { forwardRef, useCallback, useImperativeHandle, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
-// TODO: fix
-// const USERS: UserInfo[] = USER_LIST as UserInfo[];
-const USERS: UserInfo[] = [];
-
-export default function UserDetail() {
-  const { id } = useParams();
-  const user = USERS.find((user) => user.id === id);
-  return (
-    <Card>
-      <CardContent>
-        <p>This is the detail page of {user?.username}</p>
-      </CardContent>
-    </Card>
-  );
+export interface DepartmentDetailModalRef {
+  open: (id?: string) => void;
+  close: () => void;
 }
+
+interface DepartmentDetailModalProps {
+  title?: string;
+}
+
+interface FormValue {
+  name: string;
+  code: string;
+  manager: UserInfo;
+  deputy: UserInfo;
+}
+
+const DepartmentDetailModal = forwardRef<
+  DepartmentDetailModalRef,
+  DepartmentDetailModalProps
+>(({ title }, ref) => {
+  const [visible, setVisible] = useState<boolean>();
+  const [departmentId, setDepartmentId] = useState<string>();
+  const form = useForm<FormValue>({});
+
+  const close = useCallback(() => {
+    setVisible(false);
+  }, []);
+
+  const handleOpen = useCallback((id?: string) => {
+    console.info(id);
+    setDepartmentId(id);
+    setVisible(true);
+  }, []);
+
+  useImperativeHandle(ref, () => ({
+    open: handleOpen,
+    close: () => close(),
+  }));
+
+  return (
+    <Dialog open={visible} onOpenChange={close}>
+      <DialogContent className="">
+        <DialogHeader>
+          <DialogTitle>
+            {(title ?? departmentId) ? 'Edit department' : 'Add Department'}
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <div className="grid grid-cols-2 items-center gap-4">
+            <FormField
+              control={form.control}
+              name="code"
+              render={({ field }) => (
+                <FormItem className="grid grid-cols-4 items-center gap-4">
+                  <FormLabel className="text-right">Label</FormLabel>
+                  <div className="col-span-3">
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </div>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="code"
+              render={({ field }) => (
+                <FormItem className="grid grid-cols-4 items-center gap-4">
+                  <FormLabel className="text-right">Label</FormLabel>
+                  <div className="col-span-3">
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </div>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="code"
+              render={({ field }) => (
+                <FormItem className="grid grid-cols-4 items-center gap-4">
+                  <FormLabel className="text-right">Label</FormLabel>
+                  <div className="col-span-3">
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </div>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="code"
+              render={({ field }) => (
+                <FormItem className="grid grid-cols-4 items-center gap-4">
+                  <FormLabel className="text-right">Label</FormLabel>
+                  <div className="col-span-3">
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </div>
+                </FormItem>
+              )}
+            />
+          </div>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+});
+
+export default DepartmentDetailModal;
